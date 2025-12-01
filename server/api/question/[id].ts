94a95b02fabd9a5272aacb2226da863614a6937e
yaobo <15170970773@163.com>
2025-12-01 21:47:26 +0800

    fix: 移除API请求URL中的端口号9000
    更新了两个API端点URL，移除了不再需要的9000端口号以简化配置

--- server/api/question/[id].ts
+++ server/api/question/[id].ts
@@ -3,7 +3,7 @@ export default defineEventHandler(async (event) => {
   if (!id) {
     throw createError({ statusCode: 400, statusMessage: "Missing id" });
   }
-  const url = `http://115.190.61.20:9000/api/question/${id}`;
+  const url = `http://115.190.61.20/api/question/${id}`;
   // 服务器到服务器的请求，没有浏览器 CORS 问题
   const result: any = await $fetch(url);
 
